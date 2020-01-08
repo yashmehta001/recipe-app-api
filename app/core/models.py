32890c483af6bc5d1d8e9d3ef08c9e5d25070788
yashmehta001 <yashmehta001@yahoo.in>
2020-01-08 16:39:19 +0530

    updated indent

--- app/core/models.py
+++ app/core/models.py
@@ -26,11 +26,12 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    "Custom email model that supports using email instead of username"
+    """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
